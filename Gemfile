d7fed1da6dd91d074c060929856f0bb2ae3018c5
Saranya Ganapathy <saranyaganapathy95@gmail.com>
2017-06-05 16:23:20 -0400

    Checkpoint 19 complete

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ end
 group :development, :test do
   gem 'rspec-rails', '~> 3.6.0'
   gem 'rails-controller-testing'
+  gem 'pry'
 end
 
 # Use SCSS for stylesheets
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-# #8
+  # #8
   let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
   describe "GET index" do
@@ -12,31 +12,83 @@ RSpec.describe PostsController, type: :controller do
 
     it "assigns [my_post] to @posts" do
       get :index
-# #9
+      # #9
       expect(assigns(:posts)).to eq([my_post])
     end
   end
 
+  describe "GET show" do
+    let(:params) { { id: my_post.id } }
 
-#  describe "GET show" do
-#    it "returns http success" do
-#      get :show
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
+    def do_request
+      get :show, params: params
+    end
 
-# describe "GET new" do
-#   it "returns http success" do
-#     get :new
-#     expect(response).to have_http_status(:success)
-#   end
-# end
+    it "returns http success" do
+      do_request
 
-#  describe "GET edit" do
-#    it "returns http success" do
-#      get :edit
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      do_request
+
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_post to @post" do
+      do_request
+
+      expect(assigns[:post]).to eq(my_post)
+    end
+  end
+
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+
+      expect(response).to have_http_status(:success)
+    end
+
+    # #2
+    it "renders the #new view" do
+      get :new
+
+      expect(response).to render_template :new
+    end
+
+    # #3
+    it "instantiates @post" do
+      get :new
+
+      expect(assigns(:post)).not_to be_nil
+    end
+  end
+
+  describe "POST create" do
+    # #4
+    it "increases the number of Post by 1" do
+      expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+    end
+
+    # #5
+    it "assigns the new post to @post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(assigns(:post)).to eq Post.last
+    end
+
+    # #6
+    it "redirects to the new post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(response).to redirect_to Post.last
+    end
+  end
+
+  #  describe "GET edit" do
+  #    it "returns http success" do
+  #      get :edit
+  #      expect(response).to have_http_status(:success)
+  #    end
+  #  end
 
 end
