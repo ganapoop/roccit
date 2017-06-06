eba687b2c95200cace985739e10f466ca63bbf9d
Saranya Ganapathy <saranyaganapathy95@gmail.com>
2017-06-06 15:33:24 -0400

    checkpoint 20 assignment complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,15 @@ posts = Post.all
   )
 end
 
+#create questions
+100.times do
+  Question.create!(
+  title: RandomData.random_sentence,
+  body: RandomData.random_paragraph,
+  resolved: false
+  )
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
