0b9020828098ee53c39bc11bba06fcf147da9ca7
Saranya Ganapathy <saranyaganapathy95@gmail.com>
2017-06-08 22:50:43 -0400

    Checkpoint 23 complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606205021) do
+ActiveRecord::Schema.define(version: 20170609022420) do
 
   create_table "advertisments", force: :cascade do |t|
     t.string "title"
@@ -57,6 +57,14 @@ ActiveRecord::Schema.define(version: 20170606205021) do
     t.string "name"
     t.boolean "public", default: true
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
