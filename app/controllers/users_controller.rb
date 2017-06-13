5d3dc54ddde92d325605fd46a853cdc8f48c5c50
Saranya Ganapathy <saranyaganapathy95@gmail.com>
2017-06-13 00:04:00 -0400

    Checkpoint 27 complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
   def create
     @user = User.new
     @user.name = params[:user][:name]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,53 @@
-require 'random_data'
-# Create Topics
- 15.times do
-   Topic.create!(
-     name:         Faker::Hipster.sentence,
-     description:  RandomData.random_paragraph
-   )
- end
- topics = Topic.all
+#Create Users
+
+  5.times do
+    user = User.create!(
+      name:     RandomData.random_name,
+      email:    Faker::Internet.email,
+      password: RandomData.random_sentence
+    )
+  end
+  users = User.all
+# Create topics
+  15.times do
+    Topic.create!(
+      name:         Faker::Hipster.sentence,
+      description:  RandomData.random_paragraph
+    )
+  end
+  topics = Topic.all
 
 
-#Create posts
-50.times do
+# Create posts
+  50.times do
 
-  Post.create!(
-    topic: topics.sample,
-    title: Faker::Hipster.sentence,
-    body:  RandomData.random_paragraph
-  )
-end
+    Post.create!(
+      user:  users.sample,
+      topic: topics.sample,
+      title: RandomData.random_sentence,
+      body:  RandomData.random_paragraph
+    )
+  end
+  posts = Post.all
 
-posts = Post.all
+  #Create comments
 
-#Create comments
+  100.times do
+    Comment.create!(
 
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
-end
+      post: posts.sample,
+      body:  RandomData.random_paragraph
+    )
+  end
+
+  user = User.first
+  user.update_attributes!(
+    email: 'saranyaganapathy95@gmail.com',
+    password: 'helloworld'
+)
 
 puts "Seed finished"
-puts "#{Topic.count} topics created"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{User.count} users created."
+puts "#{Topic.count} topics created."
+puts "#{Post.count} posts created."
+puts "#{Comment.count} comments created."
