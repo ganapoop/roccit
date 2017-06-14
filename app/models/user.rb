682b06ebacc4bae1f3f5486e720cc746a0bd9ff1
Saranya Ganapathy <saranyaganapathy95@gmail.com>
2017-06-14 19:53:19 -0400

    Checkpoint 29 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   include BCrypt
   has_many :posts
+  has_many :comments
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
