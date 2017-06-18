7b20145b77cb07c86e0b529907c4cb7d91e24250
Saranya Ganapathy <saranyaganapathy95@gmail.com>
2017-06-18 16:27:12 -0400

    Checkpoint 32 assignment complete

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe Comment, type: :model do
   let(:topic) { create(:topic) }
   let(:user) { create(:user) }
   let(:post) { create(:post) }
-  let(:comment) { Comment.create!(body: 'Comment Body', post: post, user: user) }
-
+  let(:comment) {comment(:comment, post: post, user: user) }
   it { is_expected.to belong_to(:post) }
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:body) }
@@ -19,7 +18,7 @@ RSpec.describe Comment, type: :model do
 
   describe "after create" do
     before do
-      @another_comment = Comment.new(body: 'Comment Body', post: post, user: user)
+    @another_comment = create(:comment, post: post, user: user)
     end
 
     it "sends an email to users who have favorited the post" do
